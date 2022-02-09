59897298338cb31880d55e2d508534be057152a4
Arun S <arunsmullappallil@gmail.com>
2022-02-10 01:19:26 +0530

    Add files via upload

---
+++ cwmsdb.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 09, 2022 at 08:46 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cwmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) DEFAULT NULL,
+  `Password` varchar(100) DEFAULT NULL,
+  `updationDate` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', 'f925916e2754e5e03f75dd58a5733251', '2020-12-10 11:18:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcarwashbooking`
+--
+
+CREATE TABLE `tblcarwashbooking` (
+  `id` int(11) NOT NULL,
+  `bookingId` bigint(10) DEFAULT NULL,
+  `packageType` varchar(120) DEFAULT NULL,
+  `carWashPoint` int(11) DEFAULT NULL,
+  `fullName` varchar(150) DEFAULT NULL,
+  `mobileNumber` bigint(12) DEFAULT NULL,
+  `email` varchar(25) NOT NULL,
+  `vno` varchar(25) NOT NULL,
+  `model` varchar(25) NOT NULL,
+  `washDate` date DEFAULT NULL,
+  `washTime` time DEFAULT NULL,
+  `message` mediumtext DEFAULT NULL,
+  `status` varchar(120) DEFAULT NULL,
+  `adminRemark` mediumtext DEFAULT NULL,
+  `paymentMode` varchar(120) DEFAULT NULL,
+  `txnNumber` varchar(120) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `lastUpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcarwashbooking`
+--
+
+INSERT INTO `tblcarwashbooking` (`id`, `bookingId`, `packageType`, `carWashPoint`, `fullName`, `mobileNumber`, `email`, `vno`, `model`, `washDate`, `washTime`, `message`, `status`, `adminRemark`, `paymentMode`, `txnNumber`, `postingDate`, `lastUpdationDate`) VALUES
+(1, 316460298, '1', 1, 'ANuj kumar', 1234567890, '', '', '', '2021-12-16', '11:45:00', 'NA', 'Completed', 'Washing completed', 'e-Wallet', '345345345', '2021-12-13 19:14:22', '2021-12-14 18:04:52'),
+(2, 647869499, '3', 2, 'Anuj kumar', 1234567890, '', '', '', '2021-12-30', '15:47:00', 'na', 'New', NULL, NULL, NULL, '2021-12-13 19:14:47', '2021-12-14 17:20:56'),
+(3, 215755984, '2', 3, 'AMit', 9874563210, '', '', '', '2021-12-19', '15:05:00', 'NA', 'New', NULL, NULL, NULL, '2021-12-13 19:16:19', NULL),
+(4, 440337019, '1', 2, 'Sarita', 6987412360, '', '', '', '2022-01-01', '19:37:00', 'NA', 'New', NULL, NULL, NULL, '2021-12-14 17:01:55', NULL),
+(5, 783971257, '2', 2, 'John Doe', 1234567890, '', '', '', '2021-12-25', '13:31:00', 'NA', 'New', NULL, NULL, NULL, '2021-12-14 19:00:44', NULL),
+(6, 328979472, '3 ', 3, 'Rahul Yadav', 1234567890, '', '', '', '2021-12-18', '10:15:00', 'NA', 'New', NULL, NULL, NULL, '2021-12-14 19:12:37', NULL),
+(7, 151983398, '1', 2, 'Sanjeev', 1234569870, '', '', '', '2021-12-15', '19:50:00', 'Car wash', 'Completed', 'Car Wash Completed', 'Debit/Credit Card', 'DSGFS72342834', '2021-12-14 19:15:28', '2021-12-14 19:15:55'),
+(9, 205524939, '2', 1, 'password', 4455544555, '', '', '', '2022-02-17', '00:32:00', '', 'New', NULL, NULL, NULL, '2022-02-09 18:59:28', NULL),
+(11, 743366561, '3 ', 1, 'password', 5555555555, '', '', '', '2022-03-04', '00:41:00', '', 'New', NULL, NULL, NULL, '2022-02-09 19:18:40', NULL),
+(12, 338251061, '3 ', 3, 'password', 4455544555, '', '', '', '2022-03-12', '00:54:00', '', 'New', NULL, NULL, NULL, '2022-02-09 19:19:22', NULL),
+(13, 458105146, '3 ', 2, 'password', 4455544555, 'john@gmail.com', '', '', '2022-02-17', '12:55:00', '', 'New', NULL, NULL, NULL, '2022-02-09 19:22:45', NULL),
+(14, 564629452, '2', 2, 'password', 4455544555, 'john@gmail.com', 'hhhhv66', 'jjjjjjkkk', '2022-02-19', '00:02:00', '', 'New', NULL, NULL, NULL, '2022-02-09 19:28:44', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblwashingpoints`
+--
+
+CREATE TABLE `tblwashingpoints` (
+  `id` int(11) NOT NULL,
+  `washingPointName` varchar(255) DEFAULT NULL,
+  `washingPointAddress` varchar(255) DEFAULT NULL,
+  `contactNumber` bigint(20) DEFAULT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblwashingpoints`
+--
+
+INSERT INTO `tblwashingpoints` (`id`, `washingPointName`, `washingPointAddress`, `contactNumber`, `creationDate`) VALUES
+(1, 'XYZ Car Washing Point', 'ABC Street New Delhi 1110001', 1236547890, '2021-12-13 16:21:20'),
+(2, 'ABC Car Washing Point', 'A3263 Sector 1- Noida 201301', 98745463210, '2021-12-13 16:22:38'),
+(3, ' Matrix Car washing Point ', 'H911 Indira Puram Ghaziabad 201017 UP', 4582365419, '2021-12-13 16:24:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcarwashbooking`
+--
+ALTER TABLE `tblcarwashbooking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `carWashPoint` (`carWashPoint`);
+
+--
+-- Indexes for table `tblwashingpoints`
+--
+ALTER TABLE `tblwashingpoints`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcarwashbooking`
+--
+ALTER TABLE `tblcarwashbooking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tblwashingpoints`
+--
+ALTER TABLE `tblwashingpoints`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tblcarwashbooking`
+--
+ALTER TABLE `tblcarwashbooking`
+  ADD CONSTRAINT `washingpointid` FOREIGN KEY (`carWashPoint`) REFERENCES `tblwashingpoints` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
