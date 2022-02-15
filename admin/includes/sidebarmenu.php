808020c7f05c302c095f785e9476da049ad44d7a
Arun S <arunsmullappallil@gmail.com>
2022-02-16 01:21:02 +0530

    Add files via upload

--- admin/includes/sidebarmenu.php
+++ admin/includes/sidebarmenu.php
@@ -7,12 +7,7 @@
 									<ul id="menu" >
 										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
 										
-									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>Washing Points</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
-										   <ul id="menu-academico-sub" >
-										   <li id="menu-academico-avaliacoes" ><a href="addcar-washpoint.php">Add</a></li>
-											<li id="menu-academico-avaliacoes" ><a href="managecar-washingpoints.php">Manage</a></li>
-										  </ul>
-										</li>
+									 
 
 	<li><a href="add-booking.php"><i class="fa fa-user" aria-hidden="true"></i>  <span>Add Car Wash Booking</span><div class="clearfix"></div></a></li>
 
@@ -24,7 +19,14 @@
 												<li id="menu-academico-avaliacoes" ><a href="all-bookings.php">All</a></li>
 										  </ul>
 										</li>
-									    
+										<li><a href="manage-enquires.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Enquiries</span><div class="clearfix"></div></a></li>
+		 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span>Pages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   
+											<li id="menu-academico-avaliacoes" ><a href="contact.php">Contact</a></li>
+										  </ul>
+										</li>	
+						    
 									
 								  </ul>
 								</div>
